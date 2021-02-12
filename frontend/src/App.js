41a9c79f918d819187bf682ae6a0abd4963416b4
Liam Carr <liam@liamcarr.com>
2021-02-12 08:19:11 -0500

    Basic Structure of home page and styles
    Created and styled the header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to Sadhana Now!</h1>
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Sadhana Now!</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 };
