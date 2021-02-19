b53c85d61b2e5b945a5cd7d776dfda6da323c393
Liam Carr <liam@liamcarr.com>
2021-02-19 11:08:09 -0500

    Create RegisterUser logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,4 +43,36 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+// @desc    Register a new user
+// @route   POST /api/users/
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { displayName, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists. ");
+  }
+
+  const user = await User.create({
+    displayName,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      displayName: user.displayName,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data.");
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
