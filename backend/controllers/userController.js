d4c3a94806ebc38a92f3cbb8fe4b5a44832fc6c1
Liam Carr <liam@liamcarr.com>
2021-02-19 10:55:42 -0500

    Add JWT logic for protected routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       displayName: user.displayName,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -23,4 +24,23 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = req.user;
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      displayName: user.displayName,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile };
