adfdbe295f19416f2b2d0edf75b4048484d8482c
Liam Carr <liam@liamcarr.com>
2021-02-19 09:44:18 -0500

    Refactor lectureRoute logic into LectureController

--- backend/routes/lectureRoutes.js
+++ backend/routes/lectureRoutes.js
@@ -1,34 +1,12 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Lecture from "../models/lectureModel.js";
+import {
+  getLectures,
+  getLecturesById,
+} from "../controllers/lectureController.js";
+
 const router = express.Router();
 
-// @desc    Fetch all lectures
-// @route   GET /api/lectures
-// @access  Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const lectures = await Lecture.find({});
-    res.json(lectures);
-  })
-);
-
-// @desc    Fetch single lecture
-// @route   GET /api/lectures/:id
-// @access  Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const lecture = await Lecture.findById(req.params.id);
-
-    if (lecture) {
-      res.json(lecture);
-    } else {
-      res.status(404);
-      throw new Error("Lecture not found");
-    }
-  })
-);
+router.route("/").get(getLectures);
+router.route("/:id").get(getLecturesById);
 
 export default router;
