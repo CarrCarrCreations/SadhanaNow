8486f808cfd4488320795a032f6b8ea809db04c7
Liam Carr <liam@liamcarr.com>
2021-03-12 12:49:09 -0500

    Write first passing tests

---
+++ backend/User/tests/mock-data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    displayName: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    displayName: "John Doe",
+    email: "john@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    displayName: "Jessica Doe",
+    email: "Jessica@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
--- backend/User/tests/unit/user.controller.test.js
+++ backend/User/tests/unit/user.controller.test.js
@@ -1,11 +1,36 @@
-import UserController from "../../controller/userControllerImpl.js";
-import UserModel from "../../repo/models/userModel.js";
+import httpMocks from "node-mocks-http";
 
-const UserService = jest.fn();
+import UserController from "../../controller/userControllerImpl.js";
+import UserService from "../../services/UserService.js";
+import users from "../mock-data/users.js";
+
+UserService.registerUser = jest.fn();
 const controller = UserController(UserService);
+
+let req, res, next;
+
+beforeEach(() => {
+  // Setup req, res, and next
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
 
 describe("UserController.registerUser", () => {
   it("should have a registerUser function", () => {
-    expect(typeof UserController.registerUser).toBe("function");
+    expect(typeof controller.registerUser).toBe("function");
+  });
+
+  it("it should call UserService.registerUser", () => {
+    const user = users[0];
+    req.body = user;
+
+    controller.registerUser(req, res, next);
+
+    expect(UserService.registerUser).toBeCalledWith({
+      displayName: user.displayName,
+      email: user.email,
+      password: user.password,
+    });
   });
 });
