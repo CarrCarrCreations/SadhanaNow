c537820df48cf8aa96b3129beb2dd5f62d960967
Liam Carr <liam@liamcarr.com>
2021-03-12 13:22:48 -0500

    Should return 201 and JSON body tests

--- backend/User/tests/unit/user.controller.test.js
+++ backend/User/tests/unit/user.controller.test.js
@@ -1,30 +1,42 @@
 import httpMocks from "node-mocks-http";
 
 import UserController from "../../controller/userControllerImpl.js";
+import User from "../../repo/models/User.js";
 import UserService from "../../services/UserService.js";
 import users from "../mock-data/users.js";
 
+let req, res, next, user;
+
 UserService.registerUser = jest.fn();
+
 const controller = UserController(UserService);
 
-let req, res, next;
+const registeredUser = {
+  _id: "604babb1d2455814bbc9392e",
+  displayName: "liam",
+  email: "testLiam@example.com",
+  isAdmin: false,
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNGJhYmIxZDI0NTU4MTRiYmM5MzkyZSIsImlhdCI6MTYxNTU3MTg5MCwiZXhwIjoxNjE4MTYzODkwfQ.x0vNHqtMx8VG9oPVrCkWFnWB2MzoJOw0BQQTZZ8XZfQ",
+};
 
 beforeEach(() => {
-  // Setup req, res, and next
   req = httpMocks.createRequest();
   res = httpMocks.createResponse();
   next = null;
 });
 
 describe("UserController.registerUser", () => {
+  beforeEach(() => {
+    user = users[0];
+    req.body = user;
+  });
+
   it("should have a registerUser function", () => {
     expect(typeof controller.registerUser).toBe("function");
   });
 
   it("it should call UserService.registerUser", () => {
-    const user = users[0];
-    req.body = user;
-
     controller.registerUser(req, res, next);
 
     expect(UserService.registerUser).toBeCalledWith({
@@ -32,5 +44,20 @@ describe("UserController.registerUser", () => {
       email: user.email,
       password: user.password,
     });
+  });
+
+  it("should return 201 response code", async () => {
+    await controller.registerUser(req, res, next);
+
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should return json body in response", async () => {
+    UserService.registerUser.mockReturnValue(registeredUser);
+
+    await controller.registerUser(req, res, next);
+
+    expect(res._getJSONData()).toStrictEqual(registeredUser);
   });
 });
