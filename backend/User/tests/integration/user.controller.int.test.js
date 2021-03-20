31c12f5696136bd7396565ba9c8cf9a90626e81b
Liam Carr <liam@liamcarr.com>
2021-03-20 14:54:57 -0400

    Integration test deleteUser

--- backend/User/tests/integration/user.controller.int.test.js
+++ backend/User/tests/integration/user.controller.int.test.js
@@ -9,8 +9,72 @@ connectDB(process.env.MONGO_TEST_URI);
 
 const endpointUrl = "/api/users/";
 const updateProfileEndPointUrl = `${endpointUrl}profile`;
+const idEndPointUrl = `${endpointUrl}:id`;
 
 let adminUser;
+
+describe(updateProfileEndPointUrl, () => {});
+describe(idEndPointUrl, () => {
+  beforeAll(async () => {
+    adminUser = await request(app).post(endpointUrl).send(users[0]);
+
+    await request(app)
+      .put(updateProfileEndPointUrl)
+      .set("Authorization", "Bearer " + adminUser.body.token)
+      .send({ isAdmin: true });
+  });
+  afterAll(async () => {
+    await UserModel.collection
+      .drop()
+      .then(() => {
+        console.log("Successfully dropped User table.");
+      })
+      .catch((error) => {
+        let message;
+        if (error.message == "ns not found")
+          message = "User collection does not exist, so it cannot be dropped.";
+        else message = error.message;
+
+        console.log(`Error dropping User collection - ${message}`);
+      });
+  });
+
+  test(`GET ${idEndPointUrl}`, async () => {
+    const newUser = await request(app).post(endpointUrl).send(users[1]);
+
+    // Send request to protected/admin endpoint using adminUser JWT Token
+    const response = await request(app)
+      .get(endpointUrl + newUser.body._id)
+      .set("Authorization", "Bearer " + adminUser.body.token);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body._id).toStrictEqual(newUser.body._id);
+  });
+
+  test(`DELETE ${idEndPointUrl}`, async () => {
+    const newUser = await request(app).post(endpointUrl).send(users[2]);
+
+    // Send request to protected/admin endpoint using adminUser JWT Token
+    const response = await request(app)
+      .delete(endpointUrl + newUser.body._id)
+      .set("Authorization", "Bearer " + adminUser.body.token);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toStrictEqual({
+      message: "User successfully removed.",
+    });
+  });
+
+  it(`GET ${idEndPointUrl} - should return 404 when non-existent user ID given`, async () => {
+    // Send request to protected/admin endpoint using adminUser JWT Token
+    const response = await request(app)
+      .get(endpointUrl + "6036907cec0ac70918837817")
+      .set("Authorization", "Bearer " + adminUser.body.token);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toStrictEqual("UserService: User not found.");
+  });
+});
 
 describe(endpointUrl, () => {
   beforeAll(async () => {
@@ -51,34 +115,12 @@ describe(endpointUrl, () => {
     expect(response.body[1].displayName).toBe(newUser.body.displayName);
   });
 
-  test("GET " + endpointUrl + ":id", async () => {
-    const newUser = await request(app).post(endpointUrl).send(users[2]);
-
-    // Send request to protected/admin endpoint using adminUser JWT Token
-    const response = await request(app)
-      .get(endpointUrl + newUser.body._id)
-      .set("Authorization", "Bearer " + adminUser.body.token);
-
-    expect(response.statusCode).toBe(200);
-    expect(response.body._id).toStrictEqual(newUser.body._id);
-  });
-
   test(`POST ${endpointUrl}`, async () => {
     const response = await request(app).post(endpointUrl).send(users[3]);
 
     expect(response.statusCode).toBe(201);
     expect(response.body.displayName).toBe(users[3].displayName);
     expect(response.body.email).toBe(users[3].email);
-  });
-
-  it(`should return 404 when non-existent user ID given with GET ${endpointUrl}:id`, async () => {
-    // Send request to protected/admin endpoint using adminUser JWT Token
-    const response = await request(app)
-      .get(endpointUrl + "6036907cec0ac70918837817")
-      .set("Authorization", "Bearer " + adminUser.body.token);
-
-    expect(response.statusCode).toBe(404);
-    expect(response.body.message).toStrictEqual("UserService: User not found.");
   });
 
   it(`should return 500 on malformed data with POST ${endpointUrl}`, async () => {
